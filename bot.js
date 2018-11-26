cee0e7c55a69ac6dcd680763995ec88667d1cc0c
w2lody <44412859+w2lody@users.noreply.github.com>
2018-11-26 18:45:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "504664685426114560"; // ايدي السررفر
-var channel = "507120713619210241";//ايدي الروم
+var server = "516248530797592577"; // ايدي السررفر
+var channel = "516249540257513483";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , ')
     },305);
