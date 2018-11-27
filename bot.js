0ffd4589841605b47d490be5bdf008c1a7e274d6
w2lody <44412859+w2lody@users.noreply.github.com>
2018-11-27 13:38:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "1";
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
 client.on('ready', async() => {
-var server = "516248530797592577"; // ايدي السررفر
-var channel = "516249540257513483";//ايدي الروم
+var server = "508240875072126997"; // ايدي السررفر
+var channel = "508667278137753600";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network , #Vm,Memo , #Vm,Relo , #Venom , Network ,')
+    client.guilds.get(server).channels.get(channel).send('#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,#vM , Relo , #vM , Memo , #Venom , Network ! ,')
     },305);
 })
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|`ADMINISTRATOR`ليس لديك صلاحيات`');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 client.login(process.env.BOT_TOKEN);
